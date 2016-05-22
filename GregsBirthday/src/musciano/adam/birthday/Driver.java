8cf3b570b20b9827c88c68fff8ac99788c346496
Adam Musciano <amusciano@gmail.com>
2016-05-22 00:04:11 -0400

    Tidying up

--- GregsBirthday/src/musciano/adam/birthday/Driver.java
+++ GregsBirthday/src/musciano/adam/birthday/Driver.java
@@ -8,6 +8,8 @@ import java.time.format.DateTimeFormatter;
 public class Driver {
 	public static void main(String []args){
 		String date,day,time;
+		String setDay="07";
+		int minuteDelay=1;
 		String []songs= {"song1","song2","song3","song4",
 						  "song5","song6","song7"};
 		MakeSound m= new MakeSound();
@@ -16,11 +18,11 @@ public class Driver {
 			date= ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
 			day= date.substring(date.length()-2);
 			time= ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME).substring(3,5);
-			
-			if(day.equals("26")&&(Integer.parseInt(time)%30==0)){
+			System.out.println(date);
+			if(day.equals(setDay)&&(Integer.parseInt(time)%minuteDelay==0)){
 				x=(int)(Math.random()*(songs.length));
 				System.out.println("Playing song"+x+", time to celebrate!");
-				m.playSound("src/musciano/adam/birthday/"+songs[x]+".wav");
+				m.playSound("musciano/adam/birthday/"+songs[x]+".wav");
 			}
 		}
 		
